810140deef35dc21a3fc158685cee312b99a49f3
Ayon Kumar Saha <kumarsaha@Ayons-MacBook-Air.local>
2025-06-04 13:37:59 +0530

    Fix environment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,9 +1,17 @@
 
-const IS_PROD = true;
-const server = IS_PROD ?
-    "https://kollabvideocallbackend.onrender.com" :
+// const IS_PROD = true;
+// const server = IS_PROD ?
+//     "https://kollabvideocallbackend.onrender.com" :
 
-    "http://localhost:8000"
+//     "http://localhost:8000"
 
+
+// export default server;
+
+const isLocalhost = window?.location?.hostname === 'localhost' || window?.location?.hostname === '127.0.0.1';
+
+const server = isLocalhost
+  ? "http://localhost:8000"
+  : "https://kollabvideocallbackend.onrender.com";
 
 export default server;
